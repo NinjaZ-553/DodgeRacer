f6aba35d8d44a701a2e8adbb6ca728ec52849d9a
Kian C <karatekidhero@gmail.com>
2017-11-29 09:31:26 +0100

    Added first version of DodgeRacer a0.1

---
+++ DodgeRacer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    // Reference to Player Rigidbody
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+
+	// Unity likes FixedUpdate better. Use when doing physics
+	void FixedUpdate ()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); // Use * Time.deltaTime to even out framerate differences
+
+        if ( Input.GetKey("d") )
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
---
+++ DodgeRacer/Assets/Scripts/Quit.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Quit : MonoBehaviour {
+
+    public void quit ()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+}
